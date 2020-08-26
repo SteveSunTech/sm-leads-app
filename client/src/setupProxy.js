2c275b8d2d27b722a155153b8bdac23b4e1e0f3f
mmk <mmk.ee.911@gmail.com>
2020-08-26 04:54:51 -0700

    2020.8.26 local modify

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,9 +3,10 @@ module.exports = function (app) {
   app.use(
     ["/api/"],
     createProxyMiddleware({
-      // target: "http://localhost:5000",
-      target: "https://smcovered.herokuapp.com/",
-      // pathRewrite: { '^/api': '' }
+      target: "http://127.0.0.1:5000/",
+      // target: "https://smcovered.herokuapp.com/",
+      secure: false,
+      pathRewrite: { '^/api': '' }
     })
   );
 };
